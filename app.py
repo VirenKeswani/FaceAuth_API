e5f43569a0bcdba8ce275ad2927046e32312ca93
Viren Keswani <virenkeswani@gmail.com>
2022-09-26 16:17:30 +0530

    added 2 layers auth

--- app.py
+++ app.py
@@ -1,4 +1,6 @@
+from asyncio.windows_events import NULL
 from importlib.resources import path
+from urllib import response
 from flask import Flask,redirect, url_for, request , Response , render_template
 import pymongo
 from flask_pymongo import PyMongo
@@ -29,8 +31,8 @@ except:
 def hello_world():
     return secrets.token_urlsafe(16)
 
-@app.route('/login', methods=['POST','GET'])
-def login():
+@app.route('/<id>/login/', methods=['POST','GET'])
+def login(id):
     if request.method=='POST':
         try:
             key= secrets.token_urlsafe(16)
@@ -38,7 +40,8 @@ def login():
             user={"name": request.form['name'],
             "password": request.form['password'],
             "email": request.form['email'],
-            "api_key": key
+            "api_key": key,
+            "company_id": id
             }
 
             path = os.path.join("./known_users", key)
@@ -53,15 +56,57 @@ def login():
             )
         except Exception as e:
             return(str(e))
-    else:
+    if request.method=='GET':
         try:
             data = list(db.users.find())
-            print(data)
-            #return Response(str(data), status=200, mimetype="application/json")
-            return render_template("login.html")
+            cmp = list(db.users.find({},{"_id": 0 ,"company_id" : 1}))
+            #if cmp does not exist in the db
+            print('\n\n',cmp[0]['company_id'],'\n\n')
+            # if len(cmp)!=0:
+            #if cmp equals to the id
+            if cmp[0]['company_id']==id:
+                print(data, "here")
+                return Response(str(data), status=200, mimetype="application/json")
+                # return render_template("login.html")
+            else:
+                throw = "No user found"
+                return Response(str(throw), status=200, mimetype="application/json")
 
         except Exception as e:
             return(str(e))
+
+
+@app.route('/clogin', methods=['POST','GET'])
+def clogin():
+    if request.method=='POST':
+        try:
+            key = secrets.token_urlsafe(8)
+            company={"cname": request.form['cname'],
+            "cpassword": request.form['cpassword'],
+            "cemail": request.form['cemail'],
+            "capi_key": key
+            }
+
+            dbRes = db.company.insert_one(company)
+            return Response(
+                response=json.dumps(
+                    {"message": "User created successfully", "id": f"{dbRes.inserted_id}"}
+                ),
+                status=200,
+                mimetype="application/json"
+            )
+        except Exception as e:
+            return(str(e))
+    if request.method=='GET':
+        try:
+            data = list(db.company.find())
+            print(data)
+            return Response(str(data), status=200, mimetype="application/json")
+            #return response(data, status=200, mimetype="application/json")
+
+        except Exception as e:
+            return(str(e))
+    
 
 if __name__ == '__main__':
     app.run(debug=True)
